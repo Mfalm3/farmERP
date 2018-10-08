d270f823067c50aeec68ccdf0f7454c6969fe31d
Joe Waithaka <waitha28ka@gmail.com>
2018-10-09 00:30:35 +0300

    Add custom login page

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,13 +2,14 @@
 <html lang="{{ app()->getLocale() }}">
 @include('partials._head')
 <body class="login">
-<div>
-    <a class="hiddenanchor" id="signup"></a>
-    <a class="hiddenanchor" id="signin"></a>
-
-    <div class="login_wrapper">
-        @yield('content')
-    </div>
+<div class="container">
+    @yield('content')    
 </div>
+<script src="js/app.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
 </body>
 </html>
