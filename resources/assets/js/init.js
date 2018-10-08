69ec7a59261c49685411e837bb2b934dc6bd3412
Joe Waithaka <waitha28ka@gmail.com>
2018-10-09 00:37:23 +0300

    Added js code to preview photo upload

--- resources/assets/js/init.js
+++ resources/assets/js/init.js
@@ -10,4 +10,21 @@ $(document).ready(function() {
         allowClear: true
     });
 
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.img-circle').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+
 });
