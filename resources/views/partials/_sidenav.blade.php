27c5b503a05626154d22704e793a690e731ea714
Joe Waithaka <waitha28ka@gmail.com>
2018-10-09 00:29:42 +0300

    Sidebar changed from scrollable to fixed

--- resources/views/partials/_sidenav.blade.php
+++ resources/views/partials/_sidenav.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-3 left_col">
+<div class="col-md-3 left_col menu_fixed">
     <div class="left_col scroll-view">
         <div class="navbar nav_title" style="border: 0;">
             <a href="{{url('/')}}" class="site_title"><i class="fa fa-paw"></i> <span>{{config('app.name')}}</span></a>
@@ -7,7 +7,7 @@
         <div class="clearfix"></div>
 
         <!-- menu profile quick info -->
-        <div class="profile">
+        <div class="profile clearfix">
             <div class="profile_pic">
                 <img src="/images/img.jpg" alt="..." class="img-circle profile_img">
             </div>
@@ -26,6 +26,41 @@
                 <h3>General</h3>
                 <ul class="nav side-menu">
                     <li><a href="{{route('dashboard')}}"><i class="fa fa-home"></i> Dashboard</a>
+                    </li>
+                    <li><a><i class="fa fa-exchange"></i>Inventory <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="/seed/stock">Seed Stock</a></li>
+                            <li><a href="/seed/inventory">In the Pipeline</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-leaf"></i>Seed Records <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="/seed/plant">Seed Plant</a></li>
+                            <li><a href="/seed/report">Seeds Report</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-cart-plus"></i>Order Records <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="/orders/create">Add Order</a></li>
+                            <li><a href="/orders">View Orders</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-line-chart"></i>Sales Records <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="/sales/create">Add Sales</a></li>
+                            <li><a href="/sales">View Sales</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-dollar"></i>Expenses <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="/expenses/create">Add Expenses</a></li>
+                            <li><a href="/expenses">View Expenses</a></li>
+                            <li><a href="/suppliers">Suppliers</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/employees"><i class="fa fa-users"></i> Employee Records</a>
+                    </li>
+                    <li><a href="/reports"><i class="fa fa-pie-chart"></i> Reports</a>
                     </li>
                     <li><a><i class="fa fa-cogs"></i>Settings <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
