dd3e6afdf6a7e8ce8bfb976a289503fc83813334
Joe Waithaka <waitha28ka@gmail.com>
2018-10-09 00:38:50 +0300

    Commented out PNotify initialization

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -5056,7 +5056,7 @@ if (typeof NProgress != 'undefined') {
 		init_DataTables();
 		init_chart_doughnut();
 		init_gauge();
-		init_PNotify();
+		//init_PNotify();
 		init_starrr();
 		init_calendar();
 		init_compose();
