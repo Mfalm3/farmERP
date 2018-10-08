ce3df4ad88cfbded4618fa3432e5e6389ba47b03
Joe Waithaka <waitha28ka@gmail.com>
2018-10-09 00:27:26 +0300

    Update resource files to be compiled by laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,8 @@ let vendors = gentelella+'vendors/';
  */
 
 mix.copy(vendors+'font-awesome/fonts/','public/fonts/');
-mix.copy(gentelella+'build/js/custom.js','resources/assets/js/');
+mix.copy(vendors+'font-awesome/fonts/','public/fonts/');
+//mix.copy(gentelella+'build/js/custom.js','resources/assets/js/');
 
 
 
@@ -28,8 +29,11 @@ mix.combine([
     vendors+'nprogress/nprogress.css',
     vendors+'bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min',
     //Now lets add the custom css
-    gentelella+'build/css/custom.css',//this is the default from gentelella
-    'resources/assets/css/mycustom.css' //All your custom css can go here
+    gentelella+'build/css/custom.css', //this is the default from gentelella
+    'resources/assets/css/mycustom.css',
+    'resources/assets/css/main.css',
+    'resources/assets/css/util.css',
+    'resources/assets/css/animate.min.css'
 
 ],'public/css/app.css');
 
@@ -43,7 +47,9 @@ mix.combine([
     vendors+'nprogress/nprogress.js',
     vendors+'icheck/icheck.js',
     //lets add custom scripts
-    'resources/assets/js/custom.js',//this default from gentelella
+    'resources/assets/js/main.js',
+    'resources/assets/js/tilt.jquery.min.js',
+    'resources/assets/js/custom.js', //this default from gentelella
     'resources/assets/js/init.js' //initialize other plugins here
 ],'public/js/app.js');
 
