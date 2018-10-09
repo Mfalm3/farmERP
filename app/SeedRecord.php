9fb61a97eeefecb295cd85697ce4b52f5793ce15
Joe Waithaka <waitha28ka@gmail.com>
2018-10-09 10:22:55 +0300

    Init Seed Model

---
+++ app/SeedRecord.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SeedRecord extends Model
+{
+    public $table = 'seeds_records';
+    public $fillable = [];
+    public $timestamps = true;
+
+}
