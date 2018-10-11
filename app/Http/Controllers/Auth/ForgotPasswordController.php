80cc02198b5fcd5cc3ff24917a76c208d9275d27
Joe Waithaka <waitha28ka@gmail.com>
2018-10-11 19:40:35 +0300

    Method override for route to password reset view

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -20,6 +20,11 @@ class ForgotPasswordController extends Controller
 
     use SendsPasswordResetEmails;
 
+    public function showLinkRequestForm()
+    {
+        return view('auth.reset');
+    }
+
     /**
      * Create a new controller instance.
      *
